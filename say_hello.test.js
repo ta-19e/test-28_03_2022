ee95d2ef267c098de0e56836a1e6afe5cf51ada2
Ingela <59916738+JoinEliza@users.noreply.github.com>
2022-02-28 10:51:42 +0200

    fixed style with eslint

--- say_hello.test.js
+++ say_hello.test.js
@@ -1,5 +1,5 @@
 const sayHello = require('./say_hello');
 
 test('if name is toomas, function returns "Hello Toomas!"', () => {
-    expect(sayHello('toomas')).toBe('Hello Toomas!')
-})
+  expect(sayHello('toomas')).toBe('Hello Toomas!');
+});
