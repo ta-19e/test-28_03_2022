4c9aa2fad50ce8b1cd6fd81faf2e15052cf23f2b
AndresTPT <Andres.Tammistu@tptlive.ee>
2022-02-28 10:22:43 +0200

    packages

--- say_hello.js
+++ say_hello.js
@@ -1,7 +1,8 @@
+var single = 'single';
 var capitalize = require('capitalize');
 
 function sayHello(name) {
-    return "Hello " + capitalize.words(name) + "!";
+    return 'Hello' + capitalize.words(name) + '!';
 }
 
 module.exports = sayHello
