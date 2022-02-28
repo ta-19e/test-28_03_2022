b7df3daaea7a409c2ab608bb511566d60a58ffb2
Ingela <59916738+JoinEliza@users.noreply.github.com>
2022-02-28 10:19:05 +0200

    added lint to package json (used previous work, link in descr.), fixed style
    https://github.com/ta-19e/tund2/blob/ingeliza-patch-2/package.json

--- example.js
+++ example.js
@@ -1,18 +1,16 @@
 const chalk = require('chalk');
 const assert = require('assert');
 
-const say_hello = require("./say_hello.js")
-
+const say_hello = require('./say_hello.js');
 
 // test how function works
-console.log(say_hello("jaan"));
+console.log(say_hello('jaan'));
 // use also colors to be more "fancy"
-console.log(chalk.yellow('mari: '), chalk.bold.green(say_hello("mari")))
-console.log(chalk.yellow(`kaja kallas: `), chalk.bold.green(say_hello("kaja kallas")));
-
+console.log(chalk.yellow('mari: '), chalk.bold.green(say_hello('mari')));
+console.log(chalk.yellow('kaja kallas: '), chalk.bold.green(say_hello('kaja kallas')));
 
 // examples of testing using assert
-assert.equal(say_hello("mari"), 'Hello Mari!');
-assert.equal(say_hello("kaja kallas"), 'Hello Kaja Kallas!')
+assert.equal(say_hello('mari'), 'Hello Mari!');
+assert.equal(say_hello('kaja kallas'), 'Hello Kaja Kallas!');
 
-assert.notEqual(say_hello("mati"), 'bye mati')
+assert.notEqual(say_hello('mati'), 'bye mati');
--- say_hello.js
+++ say_hello.js
@@ -1,7 +1,7 @@
-var capitalize = require('capitalize');
+const capitalize = require('capitalize');
 
 function sayHello(name) {
-    return "Hello " + capitalize.words(name) + "!";
+  return `Hello ${capitalize.words(name)}!`;
 }
 
-module.exports = sayHello
+module.exports = sayHello;
