8ea51bf8c17c1860b4e5d50fcb09ea9bc8583d48
Ingela <59916738+JoinEliza@users.noreply.github.com>
2022-02-28 10:28:35 +0200

    fixed camelcase

--- example.js
+++ example.js
@@ -1,16 +1,16 @@
 const chalk = require('chalk');
 const assert = require('assert');
 
-const say_hello = require('./say_hello.js');
+const sayHello = require('./say_hello.js');
 
 // test how function works
 console.log(say_hello('jaan'));
 // use also colors to be more "fancy"
-console.log(chalk.yellow('mari: '), chalk.bold.green(say_hello('mari')));
-console.log(chalk.yellow('kaja kallas: '), chalk.bold.green(say_hello('kaja kallas')));
+console.log(chalk.yellow('mari: '), chalk.bold.green(sayHello('mari')));
+console.log(chalk.yellow('kaja kallas: '), chalk.bold.green(sayHello('kaja kallas')));
 
 // examples of testing using assert
-assert.equal(say_hello('mari'), 'Hello Mari!');
-assert.equal(say_hello('kaja kallas'), 'Hello Kaja Kallas!');
+assert.equal(sayHello('mari'), 'Hello Mari!');
+assert.equal(sayHello('kaja kallas'), 'Hello Kaja Kallas!');
 
-assert.notEqual(say_hello('mati'), 'bye mati');
+assert.notEqual(sayHello('mati'), 'bye mati');
