c2f389c59a164c34f7e7b9ffb6d9abe7e68b5250
Siim Tiilen <siim.tiilen@tptlive.ee>
2022-03-01 14:45:08 +0200

    added jest env to eslint and fixed issues in tests

--- say_hello.test.js
+++ say_hello.test.js
@@ -1,12 +1,13 @@
-const say_hello = require("./say_hello");
-describe( 'Say hello', ()=> {
-    test('Jan Kängsepp', ()=> {
-        expect(say_hello('jan kängsepp')).toBe("Hello Jan Kängsepp!")  
-    })
-    test('Artur Liimann', ()=> {
-        expect(say_hello('artur liimann')).toBe("Hello Artur Liimann!")  
-    })
-    test('Harri Allmann', ()=> {
-        expect(say_hello('harri allmann')).toBe("Hello Harri Allmann!")  
-    })
-})
+const sayHello = require('./say_hello');
+
+describe('Say hello', () => {
+  test('Jan Kängsepp', () => {
+    expect(sayHello('jan kängsepp')).toBe('Hello Jan Kängsepp!');
+  });
+  test('Artur Liimann', () => {
+    expect(sayHello('artur liimann')).toBe('Hello Artur Liimann!');
+  });
+  test('Harri Allmann', () => {
+    expect(sayHello('harri allmann')).toBe('Hello Harri Allmann!');
+  });
+});
